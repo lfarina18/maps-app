12a179484a77836c5e5e2641ede4ed2e570b06ef
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-07-06 14:01:48 -0300

    Context and Reducer added

---
+++ src/context/places/placesReducer.ts
@@ -0,0 +1,22 @@
+import { PlacesState } from "./PlacesProvider";
+
+
+type PlacesAction = {
+    type: 'setUserLocation',
+    payload: [number, number]
+}
+
+
+export const palcesReducer = (state: PlacesState, action: PlacesAction): PlacesState => {
+
+    switch (action.type) {
+        case 'setUserLocation':
+            return {
+                ...state,
+                isLoading: false,
+                userLocation: action.payload
+            }
+        default:
+            return state;
+    }
+}
