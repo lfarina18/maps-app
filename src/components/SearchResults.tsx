b403203a0fcd4c80563d16114e20ef3ad1c28ac0
Leonardo Farina <leonardoagustinfarina@gmail.com>
2022-07-07 17:05:54 -0300

    flight to location

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,50 @@
+import { useContext, useState } from 'react';
+import { MapContext, PlacesContext } from '../context';
+import { LoadingPLaces } from '.';
+import { Feature } from '../interfaces/Places';
+
+
+export const SearchResults = () => {
+  const { places, isLoadingPlaces } = useContext(PlacesContext);
+
+  const { map } = useContext(MapContext);
+
+  const [activeId, setActiveId] = useState('');
+
+  const onPlaceClicked = (place: Feature ) => {
+    setActiveId(place.id);
+    const [lng, lat] = place.center
+    map?.flyTo({
+      center: [lng, lat],
+      zoom: 14,
+    });
+  
+  }
+
+  if (isLoadingPlaces) {
+    return <LoadingPLaces />;
+  }
+
+  if(!places.length) {
+    return <></>;
+  }
+  
+  return (
+    <ul className="list-group mt-3">
+      {places.map((place) => (
+        <li key={place.id} 
+          className={`pointer list-group-item-action border border-light mb-2 bg-secondary p-2 text-dark  ${activeId === place.id ? 'bg-opacity-25' : 'bg-opacity-10'}`}
+          onClick={() => onPlaceClicked(place)}
+        >
+          <h6>{place.text_es}</h6>
+          <p style={{ fontSize: '12px' }}>
+            {place.place_name}
+          </p>
+          <button className={`btn btn-sm ${activeId === place.id ?  'btn-outline-primary' : 'btn-outline=dark' }`}>
+            Direcciones
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
